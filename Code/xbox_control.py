5f2609eab159502d7c4a8e1188231ea9aced0981
Sam Schoedel <seschoedel@gmail.com>
2023-05-04 11:41:32 -0500

    Fix string assignment error

--- Code/xbox_control.py
+++ Code/xbox_control.py
@@ -9,20 +9,39 @@ xb = XboxController()
 
 numb = 1
 
+max_js_value = 32767.0
+
 while True:
     data = "0512051205120512051205120512051205120000"
     data_v = list(data)
-    
+
     # Get xbox data
     left_x_raw, left_y_raw, right_x_raw, right_y_raw = xb.read()
-    x_vel = ( (left_x_raw - 32767) / 32768.0 ) * 1024
-    y_vel = ( (left_y_raw - 32767) / 32768.0 ) * 1024
-    rot_vel = ( (right_x_raw - 32767) / 32768.0 ) * 1024
-    _ = ( (right_y_raw - 32767) / 32768.0 ) * 1024
-    data[24:28] = ''.join(str(x_vel))
-    data[28:32] = ''.join(str(y_vel))
-    data[32:36] = ''.join(str(rot_vel))
-    data[36:40] = 1 # for walking mode
+    x_vel = int( left_x_raw / max_js_value * 1024)
+    y_vel = int( left_y_raw / max_js_value * 1024)
+    rot_vel = int( right_x_raw / max_js_value * 1024)
+    _ = int( right_y_raw / max_js_value * 1024)
+
+    # Prepare data for sending (convert int to string, pad with zeros until length=4)
+    x_vel_str = str(x_vel)
+    while len(x_vel_str) < 4:
+        x_vel_str = "0" + x_vel_str
+    y_vel_str = str(y_vel)
+    while len(y_vel_str) < 4:
+        y_vel_str = "0" + y_vel_str
+    rot_vel_str = str(rot_vel)
+    while len(rot_vel_str) < 4:
+        rot_vel_str = "0" + rot_vel_str
+    
+    # Convert data string to list of characters
+    data_l = list(data)
+    data_l[24:28] = x_vel_str
+    data_l[28:32] = y_vel_str
+    data_l[32:36] = rot_vel_str
+    data_l[39] = str(1) # for walking mode
+
+    # Convert data list back to one long string
+    data = ''.join(data_l)
 
     expected_data = 40
 
